634a6994cea07d71978e47e062303375aa5cf372
shuanz <shuanz@gmail.com>
2015-08-18 21:18:17 -0400

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   nc_alter.py
    	modified:   simple_udp_client.py

--- simple_udp_client.py
+++ simple_udp_client.py
@@ -7,6 +7,6 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 client.sendto("AAABBBCCC",(target_host, target_port))
 
-data,addr = client.recvfrom(4096)
+data, addr = client.recvfrom(4096)
 
 print data
