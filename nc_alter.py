dd817b1864898656fd98d70d42e4b9649405d672
shuanz <shuanz@gmail.com>
2015-08-19 19:57:08 -0400

    modified:   nc_alter.py

--- nc_alter.py
+++ nc_alter.py
@@ -14,15 +14,11 @@ port = 0
 
 def usage():
 	print "BHP Net Tool"
-	print
 	print "Usage: bhpnet.py -t target_host -p port"
-	print """-l --listen				- listen on [host]:[port] for
-											incoming connections"""
-	print """-e --execute=file_to_run	- execute the given file upon
-											receiving a connection"""
-	print "-c --command					- initialize a command shell"
-	print """-u --upload=destination 	-upon receiving connection upload a
-											file and write to [destination]"""
+	print """-l --listen: listen on [host]:[port] for incoming connections"""
+	print """-e --execute=file_to_run: execute the given file upon receiving a connection"""
+	print "-c --command: initialize a command shell"
+	print """-u --upload=destination: upon receiving connection upload a file and write to [destination]"""
 	print
 	print
 	print "Examples: "
@@ -31,6 +27,33 @@ def usage():
 	print "bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passswd\""
 	print "echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135"
 	sys.exit(0)	
+
+def client_sender(buffer):
+	client = socket.socket(socket.AF_INET, scoket.SOCK_STREAM)
+
+	try:
+		client.connect((target,port))
+		if len(buffer):
+			client.send(buffer)
+		while True:
+			recv_len = 1
+			response = ""
+
+			while recv_len:
+				data = client.recv(4096)
+				recv_len = len(data)
+				response += data
+
+				if recv_len < 4096:
+					break
+			print response,
+			buffer = raw_input("")
+			buffer += "\n"
+			client.send(buffer)
+
+	except:
+		print "[*] Exception! Exiting."
+		client.close()
 
 def main():
 	global listen
@@ -67,3 +90,12 @@ def main():
 			port = int(a)
 		else:
 			assert False, "Unhandled Option"
+
+	if not listen and len(target) and port > 0:
+		buffer = sys.stdin.read()
+		client_sender(buffer)
+
+	if listen:
+		server_loop()
+main()
+
